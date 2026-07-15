5096669aaf51b116f8d338b670b868aed1534a82
AOM <notificacoes.aom@gmail.com>
2026-07-15 23:39:14 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // <-- Confirma que tens esta linha
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/aom/',
-  plugins: [react()],
+  base: './',
+  plugins: [
+    react(),
+    tailwindcss(), // <-- Confirma que tens isto aqui dentro
+  ],
 })
