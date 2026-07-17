5bc87071503396ba615cbd0669602957472fb593
AOM <notificacoes.aom@gmail.com>
2026-07-17 16:44:15 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
-import Sobre from "./components/Sobre";
+import Associacao from "./components/Associacao"; // Renomeámos de Sobre para Associacao
 import Vantagens from "./components/Vantagens";
 import Anilhas from "./components/Anilhas";
 import Exposicoes from "./components/Exposicoes";
 import Footer from "./components/Footer";
+
+// Quando quiseres criar estas secções, basta descomentar:
+// import Criacao from "./components/Criacao";
+// import Biblioteca from "./components/Biblioteca";
 
 export default function App() {
   return (
@@ -12,10 +16,14 @@ export default function App() {
       <Header />
       <main>
         <Hero />
-        <Sobre />
+        <Associacao /> {/* Componente renomeado */}
         <Vantagens />
         <Anilhas />
         <Exposicoes />
+        
+        {/* Quando criares os ficheiros, basta adicionar aqui: */}
+        {/* <Criacao /> */}
+        {/* <Biblioteca /> */}
       </main>
       <Footer />
     </div>
