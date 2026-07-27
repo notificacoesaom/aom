968ac82bc99f54a132102b80018b7bd0ad1fbb1a
AOM <notificacoes.aom@gmail.com>
2026-07-27 18:34:03 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Associacao from "./components/Associacao"; // Renomeámos de Sobre para Associacao
-import Vantagens from "./components/Vantagens";
+//import Vantagens from "./components/Vantagens";
 import Anilhas from "./components/Anilhas";
 import Exposicoes from "./components/Exposicoes"; // Secção de destaque / countdown rápida
 import ExposicaoSection from "./components/ExposicaoSection"; // Nova secção detalhada baseada em dados
